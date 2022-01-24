9cfe8e13f2a2f36e6f23b37dbc35e48cc0c23ffe
Hsin <jessicalow2526@gmail.com>
2022-01-24 11:48:13 +0000

    task1B 1st commit

---
+++ Task1B.py
@@ -0,0 +1,43 @@
+from fileinput import close
+from floodsystem.geo import stations_by_distance
+from floodsystem.stationdata import build_station_list
+
+def run(p):
+    '''requirements for task1B'''
+    closest_list=[]
+    furthest_list=[]
+    #build list of stations
+    stations = build_station_list()
+    #list of tuple(station,distance) sorted in ascending order
+    list=stations_by_distance(stations,p)
+
+    #extracts town name from stations list by checking if the stations name are equal
+    closest_list=[(x[0],y.town,x[1]) for x in list[:10] for y in stations if x[0]==y.name]
+    furthest_list=[(x[0],y.town,x[1]) for x in list[-10:] for y in stations if x[0]==y.name]
+   
+  
+    #to extract town name from list of stations if station name matches
+    #for 10 closest station
+    # for x in list[:10]:
+    #     for y in stations:
+    #         if x[0] == y.name:
+    #             town=y.town
+    #     #build tuple of (station name, town, distance )        
+    #     final_tuple=(x[0],town,x[1]) 
+    #     closest_list.append(final_tuple)
+    
+    # #for 10 furthest station
+    # for x in list[-10:]:
+    #     for y in stations:
+    #         if x[0] == y.name:
+    #             town=y.town
+    #     #build tuple of (station name, town, distance )        
+    #     final_tuple=(x[0],town,x[1])
+    #     furthest_list.append(final_tuple) 
+
+    
+    print('The 10 closest station from Cambridge city centre is {}\n'.format(closest_list))
+    print('The 10 furthest station from Cambridge city centre is {}'.format(furthest_list))
+   
+    
+run((52.2053,0.1218))
--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -7,3 +7,83 @@ geographical data.
 """
 
 from .utils import sorted_by_key  # noqa
+from .station import MonitoringStation
+from haversine import haversine, Unit
+from operator import itemgetter
+
+def stations_by_distance(stations,p):
+    #this function calculates the distance between stations 
+    # and coordinate and forms a list of tuples sorted by distance
+    distance_list=[]
+    for i in stations:
+        distance= float(haversine(i.coord,p))
+        dist_tuple=(i.name,distance)
+        distance_list.append(dist_tuple)
+        
+    sorted_dlist=sorted_by_key(distance_list,1)
+    
+    return sorted_dlist
+
+def stations_within_radius(stations,centre,r):
+    within_r_list=[]
+    for i in stations:
+        distance= float(haversine(MonitoringStation(i).coord,centre))
+        if float(distance) < float(r):
+            within_r_list.append(i,distance)
+
+    return within_r_list
+
+def rivers_with_station(stations):
+
+    river_set={}
+    for i in stations:
+        river=i.river
+        river_set.add(river)
+    return river_set
+
+def stations_by_river(stations):
+    river_dict={}
+    for i in stations:
+        station_objects=[]
+        station_id=i.station_id
+        measure_id=i.measure_id
+        name=i.name
+        coord=i.coord
+        typical_range=i.typical_range
+        town=i.town
+        station_objects.append(station_id,measure_id,name,coord,typical_range,town)
+        river_dict[i.river]=(station_objects)
+    
+    return river_dict
+
+def rivers_by_station_number(stations,N):
+    '''determines the N rivers with the greatest number of monitoring stations,returns list of tuples sorted 
+    by number of stations'''
+    river_counter_list=[]
+    
+    for i in stations:
+        counter=0
+        river=i.river
+        for u in river_counter_list:
+            if river in u:
+                river_counter_list.remove(u)
+                counter+=1
+                river_tuple=(river,counter)
+                river_counter_list.append(river_tuple)
+            else:
+                counter=1
+                river_tuple=(river,counter)
+                river_counter_list.append(river_tuple)
+            
+    sorted(river_counter_list,key=itemgetter(1))
+    final_list=[]
+    for x in len(stations):
+        for y in river_counter_list:
+            if y[1] in river.counter_list[x]== y[1] in river.counter_list[x+1] :
+                final_list.append(y)
+
+            else:
+                final_list= river_counter_list[0,N] 
+    
+    return final_list
+
