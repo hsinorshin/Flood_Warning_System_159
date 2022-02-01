87c35bb06931b19bae513c5fb863f3a69d4f4aa2
Hsin <jessicalow2526@gmail.com>
2022-02-01 10:35:49 +0000

    finished writing test for test_station.py

--- test_station.py
+++ test_station.py
@@ -3,8 +3,33 @@
 # SPDX-License-Identifier: MIT
 """Unit test for the station module"""
 
-from floodsystem.station import MonitoringStation
-
+from floodsystem.station import MonitoringStation, inconsistent_typical_range_stations
+#created test stations
+s1 = MonitoringStation(
+    station_id=10,
+    measure_id=1,
+    label='label1',
+    coord=(0.1, 0.1),
+    typical_range=None,
+    river='river1',
+    town='town1')
+    
+s2 = MonitoringStation(
+    station_id=10,
+    measure_id=1,
+    label='label1',
+    coord=(0.1, 0.1),
+    typical_range=(1,0),
+    river='river1',
+    town='town1')
+s3 = MonitoringStation(
+    station_id=10,
+    measure_id=1,
+    label='label1',
+    coord=(0.1, 0.1),
+    typical_range=(0,1),
+    river='river1',
+    town='town1')
 
 def test_create_monitoring_station():
 
@@ -26,8 +51,22 @@ def test_create_monitoring_station():
     assert s.river == river
     assert s.town == town
 
-#def test_repr():
+def test_typical_range_consistent():
+    #test that the correct boolean returned for typical_range_consistent method
+    r1=MonitoringStation.typical_range_consistent(s1)
+    r2=MonitoringStation.typical_range_consistent(s2)
+    r3=MonitoringStation.typical_range_consistent(s3)
 
-#def test_typical_range_consistent():
+    assert r1== False
+    assert r2==False
+    assert r3 == True
+    
 
-#def test_inconsistent_typical_range_stations():
+def test_inconsistent_typical_range_stations():
+    list= [s1,s2,s3]
+    inconsistent= inconsistent_typical_range_stations(list)
+    
+    assert s1 in inconsistent
+    assert s2 in inconsistent
+    assert s3 not in inconsistent 
+
