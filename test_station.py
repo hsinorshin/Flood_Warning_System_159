f68cd2e787a75d7ea2e8b4245eb7d8b66125c2cd
Alexdggb <98261604+Definitely-Not-The-Commander@users.noreply.github.com>
2022-02-04 15:40:19 +0000

    Update test_station.py

--- test_station.py
+++ test_station.py
@@ -4,20 +4,16 @@
 """Unit test for the station module"""
 
 from floodsystem.station import MonitoringStation
-
+s_id = "test-s-id"
+m_id = "test-m-id"
+label = "some station"
+coord = (-2.0, 4.0)
+trange = (-2.3, 3.4445)
+river = "River X"
+town = "My Town
+s = MonitoringStation(s_id, m_id, label, coord, trange, river, town)
 
 def test_create_monitoring_station():
-
-    # Create a station
-    s_id = "test-s-id"
-    m_id = "test-m-id"
-    label = "some station"
-    coord = (-2.0, 4.0)
-    trange = (-2.3, 3.4445)
-    river = "River X"
-    town = "My Town"
-    s = MonitoringStation(s_id, m_id, label, coord, trange, river, town)
-
     assert s.station_id == s_id
     assert s.measure_id == m_id
     assert s.name == label
